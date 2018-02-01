7250199c53b44bf913ba29d74ee1cb50c7c16558
phani87 <phanivasudev@gmail.com>
2018-02-01 13:18:40 -0500

    Fixed Dilevey not found error

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ const UberRUSHClient = UberRUSH.createClient({
     client_secret: 'tpfmHr20Q2PNYsYyghHsl5x-eHRKDm2NCJXf8EY-',
     client_id: 'x12ZWFymkcThH2NkgkPxWQt3Sv4mPpNk',
     sandbox: true,
-    simulate : false
+    simulate: false
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+var uber_delivery = "";
 
 //get all quotes for a location 
 // post request with the payload : 
@@ -59,71 +60,76 @@ app.use(bodyParser.json());
 // 	}
 // }
 
-app.post('/api/getAQ', function(request, response) {
-
-    var uber_delivery = request.body;
-    console.log(uber_delivery);
+app.post('/api/getAQ', function (request, response) {
+    uber_delivery = request.body;
     var delivery = UberRUSHClient.createDelivery(uber_delivery);
-var allQs = '';
-delivery.quote()
-.then(function(quotes) {
-    console.log('Received ' + quotes.length + ' quotes:');
-	for (var i = 0; i < quotes.length; i++) {
-        console.log(quotes[i]);
-    }
-    
-    response.json(JSON.stringify(quotes));
-});
+    var allQs = '';
+    delivery.quote()
+        .then(function (quotes) {
+            console.log('Received ' + quotes.length + ' quotes:');
+            for (var i = 0; i < quotes.length; i++) {
+                console.log(quotes[i]);
+            }
+
+            response.json(JSON.stringify(quotes));
+        });
 });
 
 
 //post with best quote to the uber rush
-app.get('/api/confirmQ/:q_id', function(request, response) {
+app.get('/api/confirmQ/:q_id', function (request, response) {
     console.log('<<Confirm Delivery>>')
-    var q_id = request.params.q_id; 
-    //var q_id = request.body.quote; 
-    console.log("Quote ID "+q_id);
-    delivery.confirm({quote_id: q_id}).then(function(deliveries){
-        
-        var json_response = '{deliver_id : '+deliveries.delivery_id +
-        ',delivery_eta : '+deliveries.dropoff.eta +
-        ',pickup_eta : '+deliveries.pickup.eta +
-        ',fee : '+deliveries.fee +
-        '}';
-        console.log("Delivery ID "+deliveries.delivery_id);
-        console.log("Delivery ETA :" +deliveries.dropoff);
-        console.log("Pickup ETA :" +deliveries.pickup);
-        console.log("Pickup Fee :" +deliveries.fee);
-        response.json(json_response);
+    var q_id = request.params.q_id;
+    var delivery = UberRUSHClient.createDelivery(uber_delivery);
+    console.log("Quote ID " + q_id);
+    delivery.confirm({ quote_id: q_id }).then(function (self) {
+
+        // var json_response = '{deliver_id : ' + self.delivery_id +
+        //     ',delivery_eta : ' + self.dropoff.eta +
+        //     ',pickup_eta : ' + self.pickup.eta +
+        //     ',fee : ' + self.fee +
+        //     '}';
+        console.log("Delivery ID " + self.delivery_id);
+        console.log("Delivery ETA :" + self.dropoff.eta);
+        console.log("Pickup ETA :" + self.pickup.eta);
+        console.log("Pickup Fee :" + self.fee);
+        response.json(self);
     });
-    
+
     //response.json(delivery.delivery_id);
 });
 
 //get status of the delivery
-app.get('/api/getStatus', function(request, response) {
-    const d_id = request.params.d_id; 
-    delivery.updateDeliveryInfo().then(function(status){
-        console.log(delivery.status);
-        response.json(delivery.status);
+app.get('/api/getStatus/:delivery_id', function (request, response) {
+    const d_id = request.params.delivery_id;
+    var delivery = UberRUSHClient.createDelivery(uber_delivery);
+    var resp = delivery.updateDeliveryInfo1(d_id).then(function (result){
+        console.log(result);
+        response.json(result);
     });
+   
+    // delivery.updateDeliveryInfo1(d_id).then(function (status) {
+    //     console.log(delivery.status);
+    //     response.json(delivery);
+    // });
 });
 
 //manully update the status for the delivery
-app.get('/api/updateStatus/:status', function(request, response) {
-    var status = request.params.status; 
+app.get('/api/updateStatus/:status', function (request, response) {
+    var status = request.params.status;
+    var delivery = UberRUSHClient.createDelivery(uber_delivery);
     console.log(status);
     delivery.updateStatus(status);
 });
 
 
 
- 
-  app.listen(PORT, function(err) {
-	if(err) {
-		console.log("Unable to launch app on the port" + PORT, err);
-	}
 
-	console.log("Running App on port" + PORT);
+app.listen(PORT, function (err) {
+    if (err) {
+        console.log("Unable to launch app on the port" + PORT, err);
+    }
+
+    console.log("Running App on port" + PORT);
 })
 
